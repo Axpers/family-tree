c8082cfec8e069c5cd43cc66887300b85f534bc7
Garik Derminjyan <20193733@etud.univ-evry.fr>
2021-05-19 14:00:04 +0200

    Modification des commentaires

--- src/Arbre.java
+++ src/Arbre.java
@@ -26,8 +26,10 @@ public class Arbre {
       tableauPersonnes.add(instancierPersonne());
       Choix();
 
-      // TAILLE DU TABLEAU
-    } else if (choixUtilisateur == 2) {
+
+    }
+    // TAILLE DU TABLEAU
+    else if (choixUtilisateur == 2) {
 
       Personne toto = new Personne("Toto", "Toto", "Toto", "Toto");
       tableauPersonnes.add(toto);
@@ -35,13 +37,15 @@ public class Arbre {
       System.out.println("Taille du tableau : " + tableauPersonnes.size());
       Choix();
 
-      // QUITTER LE PROGRAMME
-    } else if (choixUtilisateur == 3) {
+    }
+    // QUITTER LE PROGRAMME
+    else if (choixUtilisateur == 3) {
       System.out.println("Vous quittez le programme");
       System.exit(0);
 
-      // MARIAGE
-    } else if (choixUtilisateur == 4) {
+    }
+    // MARIAGE
+    else if (choixUtilisateur == 4) {
       System.out.println("Quel est la première personne ?");
       String personne1 = lecteur.nextLine();
 
@@ -95,11 +99,12 @@ public class Arbre {
   }
 
   private static Personne trouverParPrenom(String prenom) {
-    for (int i = 0; i < tableauPersonnes.size(); i++) {
-      if (tableauPersonnes.get(i).prenom.equals(prenom)) {
-        return tableauPersonnes.get(i);
+    for (Personne tableauPersonne : tableauPersonnes) {
+      if (tableauPersonne.prenom.equals(prenom)) {
+        return tableauPersonne;
       }
     }
+    System.out.println("Cette personne n'existe pas, vous pourrez la créer.");
     return instancierPersonne();
   }
 
